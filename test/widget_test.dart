59124d192158330d5c6f88c527b80b33f98e6dd0
zhkhnn <zhakhanaruzhan@gmail.com>
2024-10-20 23:11:27 +0500

    clean

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:open_ai/main.dart';
 import 'package:open_ai/screen/chat_screen.dart';
 
 void main() {
